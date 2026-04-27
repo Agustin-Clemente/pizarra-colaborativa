69e7d7d19782584874040b2b25af7b9b51a5c9ab
Agush <a_clemente@live.com.ar>
2026-04-26 21:39:42 -0300

    cambio require a import

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
-const path = require('path');
+import express, { static } from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+import { join } from 'path';
 
 const app = express();
 app.use(cors());
 
-const server = http.createServer(app);
+const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*", 
@@ -33,12 +33,12 @@ io.on('connection', (socket) => {
 
 
 
-const publicPath = path.join(__dirname, '..', 'dist');
+const publicPath = join(__dirname, '..', 'dist');
 
-app.use(express.static(publicPath));
+app.use(static(publicPath));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+  res.sendFile(join(publicPath, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
