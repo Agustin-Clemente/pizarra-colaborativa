7567480f56988ca3384c961cfe1db98d1af37a84
Agush <a_clemente@live.com.ar>
2026-04-26 21:43:05 -0300

    Corregido server para deploy

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
-import express, { static } from 'express';
-import { createServer } from 'http';
+import express from 'express';
+import http from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
-import { join } from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
 
-const server = createServer(app);
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*", 
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
@@ -31,14 +35,12 @@ io.on('connection', (socket) => {
     });
 });
 
+const publicPath = path.join(__dirname, '..', 'dist');
 
-
-const publicPath = join(__dirname, '..', 'dist');
-
-app.use(static(publicPath));
+app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-  res.sendFile(join(publicPath, 'index.html'));
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
